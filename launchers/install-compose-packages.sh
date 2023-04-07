63eb0b8295df848b4eb0511e06513de029f62fe6
Andrea F. Daniele <afdaniele@duckietown.com>
2023-04-07 15:27:00 -0400

    working on sandbox

---
+++ launchers/install-compose-packages.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+source /environment.sh
+
+# initialize launch file
+dt-launchfile-init
+
+# YOUR CODE BELOW THIS LINE
+# ----------------------------------------------------------------------------
+
+# constants
+PACKAGE_MANAGER=${COMPOSE_DIR}/public_html/system/lib/python/compose/package_manager.py
+
+# install compose dependencies
+PACKAGES=$(awk -F: '/^[^#]/ { print $1 }' ${DT_REPO_PATH}/dependencies-compose.txt | uniq)
+python3 \
+    ${PACKAGE_MANAGER} \
+    --install ${PACKAGES}
+
+
+# ----------------------------------------------------------------------------
+# YOUR CODE ABOVE THIS LINE
+
+# wait for app to end
+dt-launchfile-join
